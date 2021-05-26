b12e0c973b83ff4208c12a7f196d6cb600bd3143
ron <rinbar70@gmail.com>
2021-05-26 23:42:15 +0300

    Change the TextField to a Select and set the color of the outline to red.

--- src/view/Home/Home.tsx
+++ src/view/Home/Home.tsx
@@ -1,15 +1,31 @@
 import React from 'react'
-import { Grid, TextField } from '@material-ui/core'
+import {
+    FormControl, Grid, InputLabel, MenuItem, Select,
+} from '@material-ui/core'
 import { makeStyles, createStyles } from '@material-ui/core/styles'
 import { useSelector, useDispatch } from 'react-redux'
 import { sessionInfoSelector, setState } from '../../storage/testSlice'
 
-const useStyles = makeStyles(() => createStyles({
+const useStyles = makeStyles((theme) => createStyles({
     container: {
         width: '100%',
     },
     placeHolder: {
         textAlign: 'center',
+    },
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 120,
+    },
+    label: {
+        '&.Mui-focused': {
+            color: 'red',
+        },
+    },
+    select: {
+        '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+            borderColor: 'red',
+        },
     },
 }))
 
@@ -17,7 +33,7 @@ const Home = (): React.ReactElement => {
     const classes = useStyles()
     const dispatch = useDispatch()
     const testState = useSelector(sessionInfoSelector)
-    const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const handleOnChange = (e: { target: { value: unknown } }) => {
         dispatch(setState(e.target.value))
     }
 
@@ -27,7 +43,14 @@ const Home = (): React.ReactElement => {
                 <h2>Home Page Placeholder:<br />state= {testState}</h2>
             </Grid>
             <Grid item xs={12} className={classes.placeHolder}>
-                <TextField label="State" value={testState} onChange={handleOnChange} />
+                <FormControl className={classes.formControl} variant="outlined">
+                    <InputLabel className={classes.label} id="state">State</InputLabel>
+                    <Select className={classes.select} labelId="state" value={testState} onChange={handleOnChange}>
+                        <MenuItem value={1}>One</MenuItem>
+                        <MenuItem value={2}>Two</MenuItem>
+                        <MenuItem value={3}>Three</MenuItem>
+                    </Select>
+                </FormControl>
             </Grid>
         </Grid>
     )
